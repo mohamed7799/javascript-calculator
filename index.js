84b2d2dc6ad0d46237746a1a10de76f2cd8c057a
mohamed7799 <mohmedamged9@gmail.com>
2018-10-12 17:15:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+let calculator = {
+  shownValue: "0",
+  currentOperation: "",
+  firstValue: 0,
+  onhold: false,
+  add: (x, y) => {
+    return x + y;
+  },
+  sub: (x, y) => {
+    return x - y;
+  },
+  mul: (x, y) => {
+    return x * y;
+  },
+  div: (x, y) => {
+    return x / y;
+  },
+  clear: function() {
+    this.shownValue = "0";
+  },
+  percentage: function() {
+    this.shownValue /= 100;
+  },
+  equal: function(currentOperation, x) {
+    this.shownValue = currentOperation(x, this.shownValue);
+  }
+};
+/* dom varables*/
+let numbers = [...document.getElementsByClassName("num-js")];
+let operations = [...document.getElementsByClassName("op-js")];
+let screen = document.getElementById("CS-js");
+let clear = document.getElementById("clear-js");
+let percentage = document.getElementById("percentage-js");
+let equal = document.getElementById("equal");
+
+let update = () => {
+  screen.innerText = calculator.shownValue;
+};
+
+clear.addEventListener("click", () => {
+  calculator.clear();
+  update();
+});
+
+percentage.addEventListener("click", () => {
+  calculator.shownValue = parseFloat(calculator.shownValue);
+  calculator.percentage();
+  update();
+});
+
+//handling operations
+operations.forEach(op => {
+  op.addEventListener("click", e => {
+    if (!calculator.onhold) {
+      calculator.currentOperation = e.target.id;
+      calculator.firstValue = parseFloat(calculator.shownValue);
+      calculator.shownValue = "0";
+    }
+
+    calculator.onhold = true;
+  });
+});
+
+//handling numbers
+numbers.forEach(element => {
+  element.addEventListener("click", event => {
+    let targetValue = event.target.innerText;
+    if (calculator.shownValue === "0") {
+      calculator.shownValue = targetValue;
+      update();
+    } else {
+      if (!(targetValue === "." && calculator.shownValue.includes("."))) {
+        calculator.shownValue += targetValue;
+        update();
+      }
+    }
+  });
+});
+
+//getting results
+equal.addEventListener("click", () => {
+  calculator.shownValue = parseFloat(calculator.shownValue);
+  calculator.equal(
+    calculator[`${calculator.currentOperation}`],
+    calculator.firstValue
+  );
+  update();
+  calculator.firstValue = 0;
+  calculator.onhold = false;
+  calculator.shownValue = "0";
+});
